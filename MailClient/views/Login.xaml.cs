db3695c55c6e01ff1ab1f44e3c37351d9705837f
whuzrurbuddha <whuzurbuddha@gmail.com>
2023-11-20 13:55:29 +0100

    Registrierung UI & Login UI

---
+++ MailClient/views/Login.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace MailClient.views;
+
+public partial class Login : UserControl
+{
+    public Login()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MailClient/views/Registration.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace MailClient.views;
+
+public partial class Registration : UserControl
+{
+    public Registration()
+    {
+        InitializeComponent();
+    }
+}
