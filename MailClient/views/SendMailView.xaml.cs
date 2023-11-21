2b501fa7735ba228558f956ebb5b68702f0b0f6b
whuzrurbuddha <whuzurbuddha@gmail.com>
2023-11-21 11:38:02 +0100

    UI/Logik für Registrierung und Login fertig. UI für EmailVersand erstellt

---
+++ MailClient/views/SendMailView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace MailClient.views;
+
+public partial class SendMailView : UserControl
+{
+    public SendMailView()
+    {
+        InitializeComponent();
+    }
+}
